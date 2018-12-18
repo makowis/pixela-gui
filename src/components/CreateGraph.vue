04b782847bf330e3b0fb26160e1be51993209651
Makoto Henmi <mako.mail.g@gmail.com>
2018-12-18 21:35:52 +0900

    フォームの設置とそれなりに見た目を整える

--- src/components/CreateGraph.vue
+++ src/components/CreateGraph.vue
@@ -1,15 +1,87 @@
 <template>
   <div>
+    <h1>Create Graph</h1>
+    <label for="username">username</label>
+    <input type="text" id="username" v-model="graphParams.username">
+    <label for="token">token</label>
+    <input type="text" id="token" v-model="graphParams.token">
+    <label for="id">id</label>
+    <input type="text" id="id" v-model="graphParams.id">
+    <label for="name">name</label>
+    <input type="text" id="name" v-model="graphParams.name">
+    <label for="unit">unit</label>
+    <input type="text" id="unit" v-model="graphParams.unit">
+    <label for="type">type</label>
+    <input type="text" id="type" v-model="graphParams.type">
+    <label for="color">color</label>
+    <input type="text" id="color" v-model="graphParams.color">
+    <label for="timezone">timezone</label>
+    <input type="text" id="timezone" v-model="graphParams.timezone">
+    <button type="button" @click="sendParams">Create</button>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
+interface GraphParams {
+  username: string,
+  token: string,
+  id: string,
+  name: string,
+  unit: string,
+  type: string,
+  color: string,
+  timezone: string,
+}
+
 @Component
 export default class CreateGraph extends Vue {
   @Prop() private msg!: string;
+
+  graphParams : GraphParams = {
+    username: '',
+    token: '',
+    id: '',
+    name: '',
+    unit: '',
+    type: '',
+    color: '',
+    timezone: ''
+  };
+
+  sendParams() {
+
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+label{
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+input {
+  display: block;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 5px;
+  margin-bottom: 15px;
+}
+
+button {
+  border: none;
+  cursor: pointer;
+  outline: none;
+  appearance: none;
+  font-weight: bold;
+  padding: 5px 10px;
+  color: white;
+  background-color: rgb(0, 132, 255); 
+  font-size: large;
+  border-radius: 5px;
+}
+</style>
