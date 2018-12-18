0e736629fb70d6d8a93a73148e4ab09752b8aaf0
Makoto Henmi <mako.mail.g@gmail.com>
2018-12-18 21:00:35 +0900

    create graph

---
+++ src/components/CreateGraph.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class CreateGraph extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <CreateGraph msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import CreateGraph from "@/components/CreateGraph.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    CreateGraph
   }
 })
 export default class Home extends Vue {}
