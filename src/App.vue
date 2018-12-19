bfd59e91a33e10afddeeb6cefd19cc9bc8a881b0
Makoto Henmi <mako.mail.g@gmail.com>
2018-12-19 22:33:13 +0900

    スタイルを調整

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view />
+    <nav id="nav">
+      <h1>Pixela GUI</h1>
+    </nav>
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+h1 {
+  color: white;
+  margin: 0;
+}
+
+nav {
+  background-color: rgb(29, 197, 6);
+  margin-bottom: 20;
+  padding: 20px;
+}
+
+main {
+  padding: 0px 20px;
+}
+</style>
+
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
